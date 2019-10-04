2e11dc0a815a2314a20ee861afac3792175935c3
siimleosk <siim.leosk@hotmail.com>
2019-10-04 09:08:03 +0300

    Test

---
+++ Muutujad/test.php
@@ -0,0 +1,16 @@
+<?php
+
+//Muutujate defineerimine ja väärtuste omistamine
+
+$nimi = 'Siim';
+$vanus = 24;
+$sugu = 'mees';
+
+//Väljasta kood tekstina
+
+echo 'Minu nimi on '.$nimi.' olen '.$sugu.' ja olen '.$vanus.' aastat vana.<br>';
+
+echo 'Vanus '.$vanus.'<br>';
+echo 'Sugu '.$sugu.'<br>';
+echo 'Nimi: '.$nimi.'<br>';
+
