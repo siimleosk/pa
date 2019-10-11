e74638d123f530e327e52a63fb92391d98e33b3a
siimleosk <siim.leosk@hotmail.com>
2019-10-11 09:36:21 +0300

    arvamismäng

--- php files/arvamismang.php
+++ php files/arvamismang.php
@@ -1,2 +1,17 @@
 <?php
-tere
+
+$etteAntud = 23;
+$kasutaja = 23;
+
+if($etteAntud == $kasutaja) {
+    echo 'Leidsid õige numbri!<br> Number on '.$kasutaja;
+} else {
+    $vahemik = $kasutaja - $etteAntud;
+    if(abs($vahemik) <= 5) {
+        echo 'Oled lähedal!<br>';
+    } if($kasutaja > $etteAntud) {
+        echo $kasutaja.' on suurem kui etteantud arv';
+    } else {
+        echo $kasutaja.' on väiksem kui etteantud arv';
+    }
+}
