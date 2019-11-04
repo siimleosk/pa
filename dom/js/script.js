7adc0ae25a56c69c7fbd14bf1b907495bcb3bd9c
siimleosk <siim.leosk@khk.ee>
2019-11-04 15:41:34 +0200

    DOM Animations

--- dom/js/script.js
+++ dom/js/script.js
@@ -1,3 +1,23 @@
 $(function () {
-    $("<h3>Siim Leosk</h3>").prependTo($("#content"));
+    var galleryImage = $(".gallery").find("img").first();
+    var images = [
+        "images/eska.jpg",
+        "images/jogi.jpg",
+        "images/laptop.jpg"
+    ];
+
+    var i = 0;
+
+    setInterval(function () {
+        i = (i + 1) % images.length;
+        galleryImage.fadeOut(function () {
+            $(this).attr("src", images[i]);
+            $(this).fadeIn();
+        });
+        console.log(galleryImage.attr("src"));
+    }, 2000);
+
+
+
+
 });
