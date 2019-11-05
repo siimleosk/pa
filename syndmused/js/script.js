1426d2129a999acd229e25995b52166d4b1a21f5
siimleosk <siim.leosk@khk.ee>
2019-11-05 14:41:08 +0200

    lightbox

--- syndmused/js/script.js
+++ syndmused/js/script.js
@@ -1,4 +1,4 @@
- $(function () {
+/* $(function () {
      $("#btn-click").click(function () {
          alert("Vajutasid nuppu.");
 
@@ -22,3 +22,26 @@
          $(this).fadeTo(500, 0.8);
      });
  });
+*/
+$(function () {
+    var galleryItems = $(".gallery").find("img");
+    galleryItems.css("width", "33%").css("opacity", "1");
+
+    galleryItems.mouseenter(function () {
+        $(this).stop().fadeTo(500, 0.7);
+    });
+    galleryItems.mouseleave(function () {
+        $(this).stop().fadeTo(500, 1);
+    });
+    galleryItems.click(function () {
+        var source = $(this).attr("src");
+        var image = $("<img>").attr("src", source).css("width", "100%");
+        $(".lightbox").empty().append(image).fadeIn(2000);
+    });
+
+    $(".lightbox").click(function () {
+        $(this).stop().fadeOut();
+    });
+
+
+});
