de19b2b6ef813594578bb80f63c4b908324597b5
siimleosk <siim.leosk@khk.ee>
2019-11-26 12:44:40 +0200

    Update script.js
    requirements

--- syndmused2/js/script.js
+++ syndmused2/js/script.js
@@ -1,10 +1,20 @@
 $(function () {
     var inputFields = $("input:text, input:password, textarea");
+
     inputFields.focus(function () {
-        $(this).css("box-shadow", "0 0 14px rgb(163, 0, 0)");
+        $(this).css("box-shadow", "0 0 4px #666");
 
     });
     inputFields.blur(function () {
         $(this).css("box-shadow", "none");
-    })
+    });
+
+    $("#name").blur(function () {
+        var text = $(this).val();
+        if (text.length < 3) {
+            $(this).css("box-shadow", "0 0 4px #811");
+        } else {
+            $(this).css("box-shadow", "0 0 4px #181");
+        }
+    });
 });
