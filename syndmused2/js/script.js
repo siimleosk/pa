68981174c7319090af17d37721e6ee02381c643d
siimleosk <siim.leosk@khk.ee>
2019-11-26 15:42:23 +0200

    parool&checkbox
    Ei saanud tööle neid millegipärast

--- syndmused2/js/script.js
+++ syndmused2/js/script.js
@@ -19,7 +19,6 @@ $(function () {
         }
     });
 });
-
 // Funktsioon nime valideerimise kohta
 function validateNameField(name, event) {
     if (!isValidName(name)) {
@@ -46,7 +45,7 @@ function validatePasswordField(password, event) {
         return password.length >= 6;
     }
 };
-
+//Funktsioon checkboxi valideerimise kohta
 function validateCheckbox(isChecked, event) {
     if (!isChecked) {
         $("#checkbox-feedback").text("Nõustu tingimustega!");
@@ -54,18 +53,74 @@ function validateCheckbox(isChecked, event) {
     } else {
         $("#checkbox-feedback").text("");
     }
+};
+
+//Funktsioon tagasisidestamiseks
+function enableFastFeedback(formElement) {
+    var nameInput = formElement.find("#name");
+    var passwordInput = formElement.find("#password");
+    var checkboxInput = formElement.find("#checkbox");
+
+    nameInput.blur(function (event) {
+        var name = $(this).val();
+        validateNameField(name, event);
+
+        if (!isValidName(name)) {
+            $(this).css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"
+            });
+        } else {
+            $(this).css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #060"
+            });
+        }
+    });
+
+    passwordInput.blur(function (event) {
+        var password = $(this).val();
+        validatePasswordField(password, event);
+
+        if (!isValidPassword(password)) {
+            $(this).css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"
+            });
+        } else {
+            $(this).css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #060"
+            });
+        }
+    });
+
+    checkboxInput.change(function (event) {
+        var isChecked = $(this).is(":checked");
+        validateCheckboxField(isChecked, event);
+
+        if (!isChecked) {
+            $(this).add("label[for='cb']").css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"
+            });
+        } else {
+            $(this).add("label[for='cb']").css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #060"
+            });
+        }
+    });
 }
 //Funktsioon vormi valideerimise kohta
 $(function () {
     $("#form").submit(function (event) {
         var name = $("#name").val();
-        var password = $("#pw").val();
+        var password = $("#password").val();
         var isChecked = $("#checkbox").is(":checked");
 
         validateNameField(name, event);
         validatePasswordField(password, event);
         validateCheckbox(isChecked, event);
-
-
     });
 });
