8789bd2bd677fa6150dc954e7ec027dc9da15a8c
siimleosk <siim.leosk@khk.ee>
2019-12-02 13:33:33 +0200

    flickr input
    Flickr api

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -1,12 +1,19 @@
 $(function () {
-    var flickrApiUrl = "https://www.flickr.com/services/feeds/photos_public.gne";
+    var flickrApiUrl = "https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     $.getJSON(flickrApiUrl, {
         tags: "sun, beach",
         tagmode: "any",
         format: "json"
     }).done(function (data) {
         console.log(data);
+        $.each(data.items, function (index, item) {
+            console.log(item);
+            $("<img>").attr("src", item.media.m).appendTo("#flickr");
+            if (index == 4) {
+                return false;
+            }
+        });
     }).fail(function () {
         alert("Ajax päring ei toimi");
     });
-})
+});
