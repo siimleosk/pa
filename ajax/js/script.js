4fb386f8df8d8553709886fdeec25db5676a1283
siimleosk <siim.leosk@khk.ee>
2019-12-02 15:19:14 +0200

    details
    pokemonide detailid konsoolis, pildid

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -1,10 +1,27 @@
 $(function () {
     var pokeApiUrl = "https://pokeapi.co/api/v2/generation/1/";
+    var pokemonByName = "https://pokeapi.co/api/v2/pokemon/";
+
     $.getJSON(pokeApiUrl).done(function (data) {
         console.log(data);
         $.each(data.pokemon_species, function (index, pokemon) {
             var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-            var par = $("<p>").html("Pokemoni liigi number " + (index + 1) + " on " + name);
+            var link = $("<a>").attr("id", pokemon.name).attr("href", "#").append($("<strong>").text(name));
+            var par = $("<p>").html("Pokemoni liigi number " + (index + 1) + " on ").append(link);
+
+            link.click(function (event) {
+                $.getJSON(pokemonByName + pokemon.name).done(function (details) {
+                    console.log(details);
+                    var pokemonDiv = $("#pokemon-details");
+                    pokemonDiv.empty();
+                    pokemonDiv.append("<h2>" + name + "</h2>");
+                    pokemonDiv.append("<img src='" + details.sprites.front_default + "'>");
+                    pokemonDiv.append("<img src='" + details.sprites.back_default + "'>");
+                    pokemonDiv.append("<img src='" + details.sprites.front_shiny + "'>");
+                    pokemonDiv.append("<img src='" + details.sprites.back_shiny + "'>");
+                });
+                event.preventDefault();
+            });
             par.appendTo("#pokemon");
         });
     }).fail(function () {
